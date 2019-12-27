fe7983f738cd87222a8a58ada705a01f13bc3a8c
vitalves <vitalvesilva@gmail.com>
2019-12-26 21:24:57 -0300

    get users github

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,30 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { Link } from 'react-router-dom';
+
+import api from '../services/axios';
+
+import './Main.css';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 
 export default function Main({ match }) {
-return (
-  <div className="main-container">
-    <img src={logo} alt="Tindev"/>
-    <ul>
-      <li>
-        <img src="https://avatars3.githubusercontent.com/u/10171839?s=460&v=4" alt="Github"/>
-        <footer>
-          <strong>Vital Alves</strong>
-          <p>Vital AlvesVital AlvesVital Alves Vital AlvesVital Alves Vital Alves Vital Alves Vital AlvesVital Alves Vital Alves</p>
-        </footer>
-        <div className="buttons">
-          <button type="button">
-            <img src={dislike} alt="dislike" />
-          </button>
-          <button type="button">
-            <img src={like} alt="like" />
-          </button>
-        </div>
-      </li>
-    </ul>
-  </div>
-);
+  const [ users, setUsers] = useState([]);
+
+  useEffect(() => {
+    (async function loadUsers() {
+      const response = await api.get('devs', {
+        headers: {
+          user: match.params.id,
+        }
+      });
+      setUsers(response.data);
+    })();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    await api.post(`devs/${id}/likes`, null, {
+      headers: {
+        user: match.params.id,
+      }
+    });
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  async function handleDislike(id) {
+    await api.post(`devs/${id}/dislikes`, null, {
+      headers: {
+        user: match.params.id,
+      }
+    });
+    setUsers(users.filter(user => user._id !== id));
+  }
+
+  const usersLength = useMemo(() => users.length, [users]);
+
+  return (
+    <div className="main-container">
+      <Link to="/">
+        <img src={logo} alt="Tindev"/>
+      </Link>
+      
+      {usersLength > 0 ? (
+        <ul>
+          {users.map(user => (
+            <li key={user._id}>
+              <img src={user.avatar} alt={user.name}/>
+              <footer>
+                <strong>{user.name}</strong>
+                <p>{user.bio}</p>
+              </footer>
+              <div className="buttons">
+                <button type="button" onClick={() => handleDislike(user._id)}>
+                  <img src={dislike} alt="dislike" />
+                </button>
+                <button type="button" onClick={() => handleLike(user._id)}>
+                  <img src={like} alt="like" />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="empty">ACABOU <br /> =( </div>
+      )}
+    </div>
+  );
 }
